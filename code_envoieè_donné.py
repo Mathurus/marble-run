a20cd6401cc1d55a29a4280b457809909e8918be
Mathurus <127137206+Mathurus@users.noreply.github.com>
2026-05-26 08:49:38 +0200

    Refactor API request and improve response handling
    Updated the API request to use a formatted URL and added response text printing.

--- code_envoieè_donné.py
+++ code_envoieè_donné.py
@@ -5,11 +5,13 @@ valeur = 20
 API_KEY = "O8HE149XGWLH8WDD"
 
 while(True):
-    r = requests.get("https://api.thingspeak.com/update?api_key={API_KEY}&field1={valeur}")
+    url = f"https://api.thingspeak.com/update?api_key={API_KEY}&field1={valeur}"
+    r = requests.get(url)
     status = r.status_code
     head = r.headers
-    if status == "200":
+    if status == 200:
         print("Envoie réussi :) ")
         print(r, head)
+        print(r.text)
     else:
         print("echec de l'envoie ")
